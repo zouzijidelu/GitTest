3b41667c9dc16a425802ba81312b9675b28a528d
zhibin wang <zhibin.wang@net263.com>
2020-08-19 16:48:16 +0800

    add model3

---
+++ GitTest/TestModel3.swift
@@ -0,0 +1,15 @@
+//
+//  TestModel0.swift
+//  GitTest
+//
+//  Created by zhibin wang on 2020/8/18.
+//  Copyright © 2020 iTalkBB. All rights reserved.
+//
+
+import UIKit
+
+class TestModel0: NSObject {
+    init() {
+        
+    }
+}
