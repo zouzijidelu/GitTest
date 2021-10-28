6256b17c9686fc87dc3f36c39020413b131dd8c8
zhibin wang <zhibin.wang@net263.com>
2021-10-28 14:19:55 +0800

    add model1

---
+++ GitTest/Model1.swift
@@ -0,0 +1,13 @@
+//
+//  Model1.swift
+//  GitTest
+//
+//  Created by zhibin wang on 2021/10/28.
+//  Copyright © 2021 iTalkBB. All rights reserved.
+//
+
+import UIKit
+
+class Model1: NSObject {
+
+}
